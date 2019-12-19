8793b50cf91f7cac02eebaa80f5a7412e13d36fe
AnujAgrawal96 <anuj.dsce18@gmail.com>
2019-12-19 20:26:59 +0530

    Initial Setup

---
+++ GooglePubApp/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace GooglePubApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+        }
+    }
+}
